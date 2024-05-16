88184e328aa7f51541a809d810e968b31d8175e9
AdrianVlad <adrianraul.vlad@gmail.com>
2024-05-16 20:07:41 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        try {
+            method1();
+        } catch (Exception e) {
+            System.out.println("My last words...");
+            throw e;
+        }
+        System.out.println("I hope this is printed");
+    }
 
-        System.out.println("Hello and welcome!");
+    public static void method1() throws Exception {
+        System.out.println("Method 1");
+        method2();
+    }
+
+    public static void method2() throws Exception {
+        System.out.println("Method 2");
+        method3();
+    }
+
+    public static void method22() throws Exception {
+        System.out.println("Method 2.2");
+        method3();
+        NullPointerException variable = new NullPointerException("I don't like you");
+        throw variable;
+    }
+
+    public static void method3() throws Exception {
+        System.out.println("Method 3");
+        throw new Exception("This is a checked exception");
     }
 }
